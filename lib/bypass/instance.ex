afd6bd40678d5952d85c061321c7f25e486de73b
Martin Schürrer <martin@schuerrer.org>
2017-09-17 14:16:20 +0200

    Enable SO_REUSEPORT to fix :eaddrinuse errors. Fixes #51.

--- lib/bypass/instance.ex
+++ lib/bypass/instance.ex
@@ -27,7 +27,7 @@ defmodule Bypass.Instance do
 
   def init([opts]) do
     # Get a free port from the OS
-    case :ranch_tcp.listen(ip: @listen_ip, port: Keyword.get(opts, :port, 0)) do
+    case :ranch_tcp.listen(so_reuseport() ++ [ip: @listen_ip, port: Keyword.get(opts, :port, 0)]) do
       {:ok, socket} ->
         {:ok, port} = :inet.port(socket)
         :erlang.port_close(socket)
@@ -264,7 +264,7 @@ defmodule Bypass.Instance do
 
   defp do_up(port, ref) do
     plug_opts = [self()]
-    {:ok, socket} = :ranch_tcp.listen(ip: @listen_ip, port: port)
+    {:ok, socket} = :ranch_tcp.listen(so_reuseport() ++ [ip: @listen_ip, port: port])
     cowboy_opts = cowboy_opts(port, ref, socket)
     {:ok, _pid} = Plug.Cowboy.http(Bypass.Plug, plug_opts, cowboy_opts)
     socket
@@ -327,6 +327,37 @@ defmodule Bypass.Instance do
     case Application.spec(:plug_cowboy, :vsn) do
       '1.' ++ _ -> [ref: ref, acceptors: 5, port: port, socket: socket]
       _ -> [ref: ref, port: port, transport_options: [num_acceptors: 5, socket: socket]]
+    end
+  end
+
+  # Use raw socket options to set SO_REUSEPORT so we fix {:error, :eaddrinuse} - where the OS errors
+  # when we attempt to listen on the same port as before, since it's still considered in use.
+  #
+  # See https://lwn.net/Articles/542629/ for details on SO_REUSEPORT.
+  #
+  # See https://github.com/aetrion/erl-dns/blob/0c8d768/src/erldns_server_sup.erl#L81 for an
+  # Erlang library using this approach.
+  #
+  # We want to do this:
+  #
+  #     int optval = 1;
+  #     setsockopt(sfd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval));
+  #
+  # Use the following C program to find the values on each OS:
+  #
+  #     #include <stdio.h>
+  #     #include <sys/socket.h>
+  #
+  #     int main() {
+  #         printf("SOL_SOCKET: %d\n", SOL_SOCKET);
+  #         printf("SO_REUSEPORT: %d\n", SO_REUSEPORT);
+  #         return 0;
+  #     }
+  defp so_reuseport() do
+    case :os.type() do
+      {:unix, :linux} -> [{:raw, 1, 15, <<1::32-native>>}]
+      {:unix, :darwin} -> [{:raw, 65535, 512, <<1::32-native>>}]
+      _ -> []
     end
   end
 end
