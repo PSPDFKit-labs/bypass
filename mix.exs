3064d016310a043f8acf8ab69820bcc58d55aac3
Daniel Neighman <dneighman@gmail.com>
2018-05-08 12:09:19 -0700

    Adds support for different plug adapters
    This is in response to using cowboy2 in an existing app but not having the ability to update the adapter in bypass (which we love!)

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule Bypass.Mixfile do
   end
 
   defp env do
-    [enable_debug_log: false]
+    [enable_debug_log: false, adapter: Plug.Adapters.Cowboy]
   end
 
   # We need to work around the fact that gun would pull in cowlib/ranch from git, while cowboy/plug
