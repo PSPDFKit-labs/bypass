107b41d59df18f1bba28afb7caa3aa9e40c0b07e
Ivy Rogatko <ivy.rogatko@therealreal.com>
2018-05-25 16:41:46 -0700

    correctly stopping server

--- lib/bypass/instance.ex
+++ lib/bypass/instance.ex
@@ -184,6 +184,11 @@ defmodule Bypass.Instance do
     end
   end
 
+
+  def handle_info(:stop, state) do
+    {:stop, :normal, state}
+  end
+
   defp do_exit(state) do
     updated_state =
       case state do
@@ -295,7 +300,7 @@ defmodule Bypass.Instance do
       if length(exit_callers) > 0 do
         {result, _updated_state} = do_exit(state)
         Enum.each(exit_callers, &(GenServer.reply(&1, result)))
-        GenServer.stop(:normal)
+        send(self(), :stop)
       end
 
       down_reset || state
