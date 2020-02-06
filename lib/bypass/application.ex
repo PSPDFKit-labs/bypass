1d9fc774f3c730f213e3a9f57251dfce3794a293
Jon Rowe <hello@jonrowe.co.uk>
2020-02-06 12:28:15 +0000

    Remove instance as a direct child

--- lib/bypass/application.ex
+++ lib/bypass/application.ex
@@ -2,10 +2,7 @@ defmodule Bypass.Application do
   use Application
 
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
     children = [
-      worker(Bypass.Instance, [], restart: :transient),
       {DynamicSupervisor, strategy: :one_for_one, name: Bypass.DynamicSupervisor}
     ]
 
