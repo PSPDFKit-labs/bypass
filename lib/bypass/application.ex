a909928ceb3bdeb7f5a4bf7144c4325dab28d065
Thiago Santos <thia.mdossantos@gmail.com>
2020-08-24 18:42:25 -0300

    Remove Supervisor.Spec import

--- lib/bypass/application.ex
+++ lib/bypass/application.ex
@@ -4,8 +4,6 @@ defmodule Bypass.Application do
   use Application
 
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
     opts = [strategy: :one_for_one, name: Bypass.Supervisor]
     DynamicSupervisor.start_link(opts)
   end
